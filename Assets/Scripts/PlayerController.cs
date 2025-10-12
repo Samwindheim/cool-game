4597016233b466305204dc757cbcf31aab49bcae
Sam Windheim <sam@windheim.org>
2025-10-12 19:09:10 +0200

    Add two-player controls

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,15 +2,38 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    public float moveSpeed = 10f;
+    public float dashForce = 20f;
+    public float dashCooldown = 2f;
+    private bool canDash = true;
+    private Rigidbody rb;
+    private Vector3 inputDir;
 
-    // Update is called once per frame
+    [SerializeField] private string horizontalAxis = "Horizontal";
+    [SerializeField] private string verticalAxis = "Vertical";
+    [SerializeField] private KeyCode dashKey = KeyCode.Space;
+
+    void Start() => rb = GetComponent<Rigidbody>();
+
     void Update()
     {
-        
+        inputDir = new Vector3(Input.GetAxisRaw(horizontalAxis), 0, Input.GetAxisRaw(verticalAxis));
+
+        if (Input.GetKeyDown(dashKey) && canDash)
+            StartCoroutine(Dash());
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 move = inputDir.normalized * moveSpeed;
+        rb.linearVelocity = new Vector3(move.x, rb.linearVelocity.y, move.z);
+    }
+
+    private System.Collections.IEnumerator Dash()
+    {
+        canDash = false;
+        rb.AddForce(inputDir.normalized * dashForce, ForceMode.Impulse);
+        yield return new WaitForSeconds(dashCooldown);
+        canDash = true;
     }
 }
