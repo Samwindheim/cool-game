c4d639b4298a6c8f43f05f993877d6160558fba2
Sam Windheim <sam@windheim.org>
2025-10-14 13:19:51 +0200

    Title screen

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -4,11 +4,31 @@ using UnityEngine.SceneManagement;
 public class UIManager : MonoBehaviour
 {
     public GameObject pauseMenu;
+    public GameObject titlePanel;
+
+    void Start()
+    {
+        // Start with the title screen active and the game paused
+        if (titlePanel != null)
+        {
+            titlePanel.SetActive(true);
+            Time.timeScale = 0;
+        }
+    }
 
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape))
             TogglePause();
+    }
+
+    public void StartGame()
+    {
+        if (titlePanel != null)
+        {
+            titlePanel.SetActive(false);
+            Time.timeScale = 1;
+        }
     }
 
     public void TogglePause()
